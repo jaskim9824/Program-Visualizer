e287f5d2422efb61d974dbb3074e73e8259d28e3
jaskim9824 <jaskim9824@gmail.com>
2022-05-24 11:17:51 -0600

    Made baseline additons to script

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from bs4 import BeautifulSoup
 
 def main ():
-    print("Hello world!")
+    #opening the template html file and constructing html
     try:
-        with open("template.html") as fp:
-            soup = BeautifulSoup(fp, 'html.parser')
-            print("File opened!")
+        with open("template.html") as input:
+            # deriving parsed html
+            soup = BeautifulSoup(input, 'html.parser')
+            #locating main div, this is where all the html will be written
+            mainTag = soup.body.find("div", id="main")
+            # print("Main")
+            # print(mainTag)
+
+            # locating form tag
+            formTag = mainTag.find("form")
+            # print("Form")
+            # print(formTag)
+
+            # TODO:  insert different radio inputs here based on plan sequence in form tag
+
+            # locating display tag, this is where the course divs will be written
+            displayTag = mainTag.find("div", class_="display")
+
+            # print("Display")
+            # print(displayTag)
+
+            # TODO: insert course divs here
+    #TO DO: improve expection handling here
     except:
         print("Exception raised")
-    
-
+    #writing output to an output html
+    try:
+        with open("template-output.html", "w") as output:
+            output.write(str(soup))
+    #TO DO: improve expection handling here
+    except:
+        print("Exception raised")
+        
 if __name__ == "__main__":
     main()
