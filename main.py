65a253c6fa5f49e56357f5fa3bd76e1f3b5a8615
Moaz Abdelmonem <moazabdelmonem7@gmail.com>
2022-05-25 15:21:12 -0600

    Hover boxes

--- main.py
+++ main.py
@@ -100,19 +100,35 @@ def placeTermsDivs(planTag, planDict, soup, courseDict):
 
 def placeCourses(termTag, termList, soup, courseDict):
     for course in termList:
+        #####ORIGINALLY#########
+        # courseContDiv = soup.new_tag("div", class_="coursecontainer")
+        # courseInfo = soup.new_tag("div", class_="courseinfo")
+        # courseDisc = soup.new_tag("p")
+        # courseDisc.append(course.course_description)
+        # #courseDisc.append("\{\{"+course.name.strip().replace(" ","")+"courseinfo\}\}")
+        # courseDiv = soup.new_tag("div", attrs= {"class":"course", "id": course.name, "ng-click":"\{\{"+course.name.strip().replace(" ", "")+"Listener()\}\}" })
+        # courseHeader = soup.new_tag("h3", class_="embed")
+        # courseHeader.append(course.name)
+        # courseInfo.append(courseDisc)
+        # courseDiv.append(courseHeader)
+        # courseContDiv.append(courseInfo)
+        # courseContDiv.append(courseDiv)
+        # termTag.append(courseContDiv)
+
+
+        #Adding the hover over boxes
         courseContDiv = soup.new_tag("div", class_="coursecontainer")
-        courseInfo = soup.new_tag("div", class_="courseinfo")
-        courseDisc = soup.new_tag("p")
-        courseDisc.append(course.course_description)
         #courseDisc.append("\{\{"+course.name.strip().replace(" ","")+"courseinfo\}\}")
-        courseDiv = soup.new_tag("div", attrs= {"class":"course", "id": course.name, "ng-click":"\{\{"+course.name.strip().replace(" ", "")+"Listener()\}\}" })
+        courseDiv = soup.new_tag("div",attrs= {"class":"course tooltip", "id": course.name, "ng-click":"\{\{"+course.name.strip().replace(" ", "")+"Listener()\}\}" })
+        courseDisc = soup.new_tag("p", class_="tooltiptext")
+        courseDisc.append(course.course_description)
         courseHeader = soup.new_tag("h3", class_="embed")
         courseHeader.append(course.name)
-        courseInfo.append(courseDisc)
+        courseDiv.append(courseDisc)
         courseDiv.append(courseHeader)
-        courseContDiv.append(courseInfo)
         courseContDiv.append(courseDiv)
         termTag.append(courseContDiv)
+
         
 
 
