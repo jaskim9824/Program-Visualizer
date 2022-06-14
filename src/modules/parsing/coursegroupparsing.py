238775f9b017f1e6363fdc563bea8507e84cb1bd
jaskim9824 <jaskim9824@gmail.com>
2022-06-14 15:20:29 -0600

    docs to new module

--- src/modules/parsing/coursegroupparsing.py
+++ src/modules/parsing/coursegroupparsing.py
@@ -6,6 +6,12 @@
 # The file contains the functions needed to extract the course group 
 # information needed to generate the web page
 
+# Function that finds the intital values to set each course group variable to
+# in the JS
+# Parameters:
+#   courseGroupDict - dict that maps plans to the course groups that exist in them
+#   courseGroupList - list of course groups taken in that program
+# Returns a dict that maps the course group with the initial value it should take
 def findIntitalValuesofCourseGroups(courseGroupDict, courseGroupList):
     intitalSelectionGroups = list(courseGroupDict.values())[0]
     intitalCourseGroupVals = {}
@@ -16,6 +22,10 @@ def findIntitalValuesofCourseGroups(courseGroupDict, courseGroupList):
             intitalCourseGroupVals[element] = intitalSelectionGroups[element][0]
     return intitalCourseGroupVals
 
+# Function that finds the list of all course groups taken that term
+# Parameters:
+#   courseGroupDict - dict that maps plans to the course groups that exist in them
+# Returns a list of course groups taken in that program
 def findListofAllCourseGroups(courseGroupDict):
     currentList = []
     for plan in courseGroupDict:
@@ -24,6 +34,11 @@ def findListofAllCourseGroups(courseGroupDict):
                 currentList.append(element)
     return currentList
 
+# Function that consturcts a dict that maps plans to the course groups existing in that plan
+# and their options
+# Parameters:
+#   sequenceDict - dict that maps plan names to the plan dict containing sequncing info about that plan
+# Returns a dict that maps plans to the course groups that exist in them
 def extractPlanCourseGroupDict(sequnceDict):
     courseGroupDict = {}
     for plan in sequnceDict:
@@ -41,6 +56,10 @@ def extractPlanCourseGroupDict(sequnceDict):
         courseGroupDict[shortenedPlanName] = appendCourseGroups(planCourseGroupsDict,courseGroupList)
     return courseGroupDict
 
+# Function that appends course groups and options to a specfic plan course group dict
+#   planCourseGroupDict - dict that maps course groups to a list of options taken in that group
+#   courseGroupList - list of course group options
+# Returns a new course group dict for that specifc plan
 def appendCourseGroups(planCourseGroupsDict, courseGroupList):
     for group in courseGroupList:
         numOfGroup = int(''.join(filter(lambda s: (s.isdigit()), group)))
@@ -50,6 +69,11 @@ def appendCourseGroups(planCourseGroupsDict, courseGroupList):
             planCourseGroupsDict[numOfGroup].append(group)
     return planCourseGroupsDict
 
+# Function that creates a list of course groups for a specfic variation of a course group
+# selection
+# Parameters:
+#   planName - long plan name of specific variant
+# Returns a list of the options chosen in that variant
 def extractCourseGroupListFromString(planName):
     index = planName.find("{")
     if index == -1:
